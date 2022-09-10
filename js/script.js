5afad9e91e8462c4910edbac8feddce3462edcb7
sqr <102550033+sqr6363@users.noreply.github.com>
2022-09-10 20:13:52 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -6,4 +6,36 @@ hamburger.addEventListener("click", function () {
   menu.classList.toggle("mobile");
 });
 
+let slideIndex = 1;
+
+let plusSlides = (n) => {
+  showSlides((slideIndex += n));
+};
+
+let currentSlide = (n) => {
+  showSlides((slideIndex = n));
+};
+
+let showSlides = (n) => {
+  let i;
+  let slides = document.getElementsByClassName("slider__image");
+  let dots = document.getElementsByClassName("slider__dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+};
+
+showSlides(slideIndex);
+
 //# sourceMappingURL=script.js.map
